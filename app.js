a9340eeee6f03f952034570498e0f61e7bf145ea
Jackson Smith <jsmithbootcamp2020@gmail.com>
2020-12-27 16:31:56 -0500

    prompts and getting roles code

--- app.js
+++ app.js
@@ -13,3 +13,74 @@ var connection = mysql.createConnection({
     password: "rootroot",
     database: "employees_db"
   });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    start();
+    getDepartments();
+    getRoles();
+    getManagers();
+    getEmployees();
+  });
+
+  start = () => {
+    inquirer
+    .prompt({
+      name: "choices",
+      type: "list",
+      message: "What would you like to do?",
+      choices: ["ADD", "VIEW", "UPDATE", "DELETE", "EXIT"]
+    })
+    .then(function(answer) {
+      if (answer.choices === "ADD") {
+        addSomething();
+      }
+      else if (answer.choices === "VIEW") {
+        viewSomething();
+      } 
+      else if (answer.choices === "UPDATE") {
+        updateSomething();
+      }
+      else if (answer.choices === "DELETE") {
+        deleteSomething();
+      }
+      else if (answer.choices === "EXIT") {
+        (err, result) => {
+          console.log(err || result);
+        };
+      
+        connection.end();
+      }
+      else{
+        connection.end();
+      }
+    });
+}
+
+getRoles = () => {
+    connection.query("SELECT id, title FROM role", (err, res) => {
+      if (err) throw err;
+      roles = res;
+    })
+  };
+  
+  getDepartments = () => {
+    connection.query("SELECT id, name FROM department", (err, res) => {
+      if (err) throw err;
+      departments = res;
+    })
+  };
+  
+  getManagers = () => {
+    connection.query("SELECT id, first_name, last_name, CONCAT_WS(' ', first_name, last_name) AS managers FROM employee", (err, res) => {
+      if (err) throw err;
+      managers = res;
+    })
+  };
+  
+  getEmployees = () => {
+    connection.query("SELECT id, CONCAT_WS(' ', first_name, last_name) AS Employee_Name FROM employee", (err, res) => {
+      if (err) throw err;
+      employees = res;
+    })
+  };
