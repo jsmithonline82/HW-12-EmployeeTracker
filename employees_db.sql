6142fc79dd076129102cebf26875ff1564abdd3b
Jackson Smith <jsmithbootcamp2020@gmail.com>
2020-12-27 16:38:45 -0500

    working on Tables

--- employees_db.sql
+++ employees_db.sql
@@ -16,3 +16,12 @@ CREATE TABLE role (
   department_id INT, 
   PRIMARY KEY (id)
 );
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR (50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  role_id INT NOT NULL, 
+  manager_id INT, 
+  PRIMARY KEY (id)
+);
