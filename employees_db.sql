8cc5e0801badce9fa3f575e1df0ac56956c03aa3
Jackson Smith <jsmithbootcamp2020@gmail.com>
2020-12-27 16:55:35 -0500

    Working with DB

--- employees_db.sql
+++ employees_db.sql
@@ -25,3 +25,14 @@ CREATE TABLE employee (
   manager_id INT, 
   PRIMARY KEY (id)
 );
+
+INSERT INTO department (name)
+VALUES ("Sales"), ("Customer Service"), ("Management"), ("Legal"), ("Accounting");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("President", "1000000", "6"), ("I.T. Department", "70000", "2"), ("Lawyer", "60000", "3"), ("Lawyer", "60000", "4"), ("Customer Service Rep", "60000", "5"), ("Salesperson", "40000", "1");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jackson", "Smith", "1"), ("Jaxon", "Smiff", "2", "1"), ("Jaksin", "Smithed", "3", "1"), ("Jake", "Smyth", "4", "3"), ("James", "Snuff", "5", "1");
+
+SELECT e.id, e.first_name, e.last_name, d.name AS department, r.title, r.salary, CONCAT_WS(" ", m.first_name, m.last_name) AS manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id ORDER BY e.id ASC;
